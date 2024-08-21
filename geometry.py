7858e94a9dc4a116ec14d159ee913adbe20162f1
Dr.A07 <157939154+NecroticFactor@users.noreply.github.com>
2024-08-22 00:33:27 +0530

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -37,30 +37,31 @@ def get_calculation(shape):
 
 # Does the calculations
 def calculator(shape, requirement):
-    try:
-        if shape == "circle":
-            radius = float(input("Enter the radius in cm: "))
-            if requirement == "circumference":
-                return circumference_circle(radius)
-            else:
-                return area_circle(radius)
+    while True:
+        try:
+            if shape == "circle":
+                radius = float(input("Enter the radius in cm: "))
+                if requirement == "circumference":
+                    return circumference_circle(radius)
+                else:
+                    return area_circle(radius)
 
-        elif shape == "rectangle":
-            length = float(input("Enter the length in cm: "))
-            width = float(input("Enter the width in cm: "))
-            if requirement == "perimeter":
-                return perimeter_rectangle(length, width)
-            else:
-                return area_rectangle(length, width)
+            elif shape == "rectangle":
+                length = float(input("Enter the length in cm: "))
+                width = float(input("Enter the width in cm: "))
+                if requirement == "perimeter":
+                    return perimeter_rectangle(length, width)
+                else:
+                    return area_rectangle(length, width)
 
-        elif shape == "square":
-            side = float(input("Enter the side in cm: "))
-            if requirement == "perimeter":
-                return perimeter_square(side)
-            else:
-                return area_square(side)
-    except ValueError:
-        print("Enter a valid number")
+            elif shape == "square":
+                side = float(input("Enter the side in cm: "))
+                if requirement == "perimeter":
+                    return perimeter_square(side)
+                else:
+                    return area_square(side)
+        except ValueError:
+            print("Enter a valid number")
 
 # Circle circumference function
 def circumference_circle(radius):
